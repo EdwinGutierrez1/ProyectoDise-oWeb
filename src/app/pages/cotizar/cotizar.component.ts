ee2cfbecfe7b416954d47c2bfc52f9f7d6abfd7b
Edwin Gutierrez <egutierrez11@udi.edu.co>
2025-05-30 19:50:33 -0500

    comienzo proceso de cotizar

--- src/app/pages/cotizar/cotizar.component.ts
+++ src/app/pages/cotizar/cotizar.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CabanasComponent } from "./cabanas/cabanas.component";
 import { FormularioComponent } from "./formulario/formulario.component";
 import { ActividadesComponent } from "./actividades/actividades.component";
+import { CotizacionService } from './cotizacion.service';
 
 @Component({
   selector: 'app-cotizar',
   standalone: true,
-  imports: [CabanasComponent, FormularioComponent, ActividadesComponent],  
+  imports: [CabanasComponent, FormularioComponent, ActividadesComponent],
   templateUrl: './cotizar.component.html',
   styleUrls: ['./cotizar.component.css']
 })
-export class CotizarComponent {}
+export class CotizarComponent implements OnInit {
+  
+  constructor(private cotizacionService: CotizacionService) { }
+
+  ngOnInit(): void {
+    // Reiniciar cotización al iniciar
+    this.cotizacionService.reiniciarCotizacion();
+    
+    // Suscribirse a cambios para debug (opcional)
+    this.cotizacionService.cotizacion$.subscribe(cotizacion => {
+      console.log('Estado actual de cotización:', cotizacion);
+    });
+  }
+}
