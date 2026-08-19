074d2d418b9282e125dce85c27e75fa89f9d4e0c
Alejandro <jmartinez75@udi.edu.co>
2026-08-18 21:11:39 -0500

    Estética menu hamburguesa corregida

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
@@ -14,5 +15,20 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     const hour = new Date().getHours();
     this.logoPath = hour >= 6 && hour < 18 ? 'logoDia.png' : 'logoNoche.png';
+  }
+
+  // Cierra el menú hamburguesa manualmente al hacer clic en cualquier link
+  cerrarMenu(): void {
+    const menu = document.getElementById('navbarSupportedContent');
+    if (menu && menu.classList.contains('show')) {
+      const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menu);
+      bsCollapse.hide();
+    }
+
+    // También reinicia el ícono animado de las 3 rayitas a su estado cerrado
+    const checkbox = document.getElementById('toggle-menu') as HTMLInputElement;
+    if (checkbox) {
+      checkbox.checked = false;
+    }
   }
 }
